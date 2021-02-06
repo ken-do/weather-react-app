d5591fa7af31b66170f43596bbf90b23dcf669e7
Ken Do <khuong.a.do@gmail.com>
2021-02-06 15:59:05 +0700

    chore(lint): ensure consistent coding style with prettier & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+    extends: [
+        'react-app',
+        'react-app/jest',
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'prettier',
+        'prettier/react',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'plugin:promise/recommended',
+    ],
+    plugins: ['react', '@typescript-eslint', 'jest', 'prettier'],
+    env: {
+        browser: true,
+        es6: true,
+        jest: true,
+    },
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    rules: {
+        'linebreak-style': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+        // warns about leftover log/debugger statements when building for Production
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        // Rules for working with promises
+        'promise/always-return': 'warn',
+        'promise/catch-or-return': 'warn',
+        'promise/prefer-await-to-then': 'warn',
+        'promise/prefer-await-to-callbacks': 'warn',
+    },
+    overrides: [
+        {
+            files: ['src/**/*.tsx'],
+            rules: {
+                '@typescript-eslint/explicit-module-boundary-types': 'off',
+            },
+        },
+    ],
+}
