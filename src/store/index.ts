7320b1ab5ed3ac23e2d0ba4bc55b00604a559bb3
Ken Do <khuong.a.do@gmail.com>
2021-02-08 11:57:54 +0700

    refactor: only apply redux-logger in the development environment

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,27 @@
 /* eslint-disable global-require, @typescript-eslint/no-var-requires */
 import { configureStore } from '@reduxjs/toolkit'
+import { Middleware } from 'redux'
 import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import rootReducer from './rootReducer'
+import rootSaga from './rootSaga'
 
 const sagaMiddleware = createSagaMiddleware()
 
+const appliedMiddlewares: Middleware[] = [sagaMiddleware]
+
+if (process.env.NODE_ENV === 'development') {
+    appliedMiddlewares.push(logger)
+}
+
 const store = configureStore({
     reducer: rootReducer,
-    middleware: [sagaMiddleware, logger] as const,
+    middleware: appliedMiddlewares,
 })
 
+sagaMiddleware.run(rootSaga)
+
+/* istanbul ignore-text */
 if (process.env.NODE_ENV === 'development' && module.hot) {
     module.hot.accept('./rootReducer', () => {
         const newRootReducer = require('./rootReducer').default
