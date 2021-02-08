5fb9e347d6d4e8554f8b741ea73ceb1e888fbc2d
Ken Do <khuong.a.do@gmail.com>
2021-02-08 12:06:50 +0700

    chore: update package.json and jest config file to run tests with coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+        './src/features/**/*.ts': {
+            branches: 100,
+            statements: 100,
+        },
+    },
     moduleDirectories: ['node_modules', 'src'],
     collectCoverageFrom: [
         'src/**/*.{ts,tsx}',
