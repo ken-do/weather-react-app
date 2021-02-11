804f730221abad4a9b674c451a70392059d96583
Ken Do <khuong.a.do@gmail.com>
2021-02-11 15:03:43 +0700

    chore: download and keep the weather images in a static folder

---
+++ src/utils/weatherForecastImages.ts
@@ -0,0 +1,25 @@
+import sn from 'static/img/weather/sn.svg'
+import sl from 'static/img/weather/sl.svg'
+import h from 'static/img/weather/h.svg'
+import t from 'static/img/weather/t.svg'
+import hr from 'static/img/weather/hr.svg'
+import lr from 'static/img/weather/lr.svg'
+import s from 'static/img/weather/s.svg'
+import hc from 'static/img/weather/hc.svg'
+import lc from 'static/img/weather/lc.svg'
+import c from 'static/img/weather/c.svg'
+
+const weatherForecastImages: Record<string, string | undefined> = {
+    sn,
+    sl,
+    h,
+    t,
+    hr,
+    lr,
+    s,
+    hc,
+    lc,
+    c,
+}
+
+export default weatherForecastImages
