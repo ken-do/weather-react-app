47eebaa0829b4412c6db51c0182070248a6fd398
Ken Do <khuong.a.do@gmail.com>
2021-02-11 16:29:47 +0700

    feat(views): add Location view

---
+++ src/router/views/Location.test.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { render, waitFor } from 'utils/testUtils'
+import store from 'store'
+import {
+    getLocationsSuccess,
+    getLocationsFailure,
+    resetLocations,
+    getLocationsStart,
+} from 'features/location/locationsSlice'
+import { search as locations } from 'server/db.json'
+import * as slice from 'features/location/locationsSlice'
+import { PATH } from 'utils/constants'
+import Location from './Location'
+
+describe('Location view', () => {
+    afterEach(() => {
+        store.dispatch(resetLocations())
+    })
+
+    test('should render a loading indicator when location data is being fetched', () => {
+        store.dispatch(getLocationsStart())
+        const { queryByRole, asFragment } = render(<Location />)
+        const loadingIndicator = queryByRole('status')
+        expect(loadingIndicator).toBeInTheDocument()
+        expect(asFragment()).toMatchSnapshot()
+    })
+
+    test('should show a list of locations after location data was', () => {
+        store.dispatch(getLocationsSuccess(locations))
+        const { queryAllByRole, asFragment } = render(<Location />)
+        const locationList = queryAllByRole('article')
+        expect(locationList).toHaveLength(locations.length)
+        expect(asFragment()).toMatchSnapshot()
+    })
+
+    test('should show a message saying there is no results by default or if the request failed to return any data', () => {
+        const { getByText, rerender, asFragment } = render(<Location />)
+        const noResultsMessage = getByText(/no results/i)
+        expect(noResultsMessage).toBeInTheDocument()
+        store.dispatch(getLocationsSuccess([]))
+        rerender(<Location />)
+        const stillNoResultsMessage = getByText(/no results/i)
+        expect(stillNoResultsMessage).toBeInTheDocument()
+        store.dispatch(getLocationsFailure(new Error('Error fetching')))
+        rerender(<Location />)
+        const stillAgainNoResultsMessage = getByText(/no results/i)
+        expect(stillAgainNoResultsMessage).toBeInTheDocument()
+        expect(asFragment()).toMatchSnapshot()
+    })
+
+    test('should fetch locations when there is a "query" param', () => {
+        const { rerender, asFragment } = render(<Location />)
+        const url = `${PATH.location}?query=san`
+        window.history.pushState({}, '', url)
+        const spy = jest.spyOn(slice, 'getLocations')
+        rerender(<Location />)
+        waitFor(() => {
+            expect(spy).toHaveBeenCalledWith()
+        })
+        spy.mockRestore()
+        expect(asFragment()).toMatchSnapshot()
+    })
+})
--- src/router/views/Location.tsx
+++ src/router/views/Location.tsx
@@ -1,5 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import LocationList from 'features/location/LocationList'
+import { useQuery } from 'utils/router'
+import { useDispatch, useSelector } from 'utils/store'
+import { getLocations } from 'features/location/locationsSlice'
+import { EmptyStateMessage, LoadingIndicator } from 'components'
 
-const Location = () => <div>Location</div>
+const Location = () => {
+    const queryParams = useQuery()
+    const query = queryParams.get('query')
+    const dispatch = useDispatch()
+    const locations = useSelector((state) => state.locations.entries)
+    const isLoading = useSelector((state) => state.locations.isLoading)
+
+    useEffect(() => {
+        if (query) {
+            dispatch(getLocations(query))
+        }
+    }, [dispatch, query])
+
+    if (isLoading) {
+        return <LoadingIndicator />
+    }
+
+    if (locations.length) {
+        return <LocationList locations={locations} />
+    }
+
+    return (
+        <EmptyStateMessage
+            heading="No Results"
+            subHeading="Try searching for another location with a different query."
+        />
+    )
+}
 
 export default Location
