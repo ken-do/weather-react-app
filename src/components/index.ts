4fa8eb7ea2bdecef4c3a2d88c2e964428db1f653
Ken Do <khuong.a.do@gmail.com>
2021-02-11 16:14:55 +0700

    chore(components): export all the components from an index.ts file

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as NavBar } from './NavBar'
+export { default as EmptyStateMessage } from './EmptyStateMessage'
+export { default as LoadingIndicator } from './LoadingIndicator'
+export { default as SearchBox } from './SearchBox'
+export { SearchBoxWithSuggestions } from './SearchBox'
