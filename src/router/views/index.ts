7cd67e30f2a35d8f12022f2efa51f16c6543b433
Ken Do <khuong.a.do@gmail.com>
2021-02-11 17:47:14 +0700

    feat(lazy): lazy load views with loadable

--- src/router/views/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home'
-export { default as NotFound } from './NotFound'
-export { default as Location } from './Location'
-export { default as Weather } from './Weather'
---
+++ src/router/views/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import loadable from '@loadable/component'
+import { LoadingIndicator } from 'components'
+
+export const Home = loadable(
+    () => import(/* webpackChunkName: "Home" */ './Home'),
+    {
+        fallback: <LoadingIndicator />,
+    }
+)
+export const NotFound = loadable(
+    () => import(/* webpackChunkName: "NotFound" */ './NotFound'),
+    {
+        fallback: <LoadingIndicator />,
+    }
+)
+export const Location = loadable(
+    () => import(/* webpackChunkName: "Location" */ './Location'),
+    {
+        fallback: <LoadingIndicator />,
+    }
+)
+export const Weather = loadable(
+    () => import(/* webpackChunkName: "Weather" */ './Weather'),
+    {
+        fallback: <LoadingIndicator />,
+    }
+)
