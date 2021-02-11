26193a7dcfb22fe29a0f7d78f8894a05b8d9d1bc
Ken Do <khuong.a.do@gmail.com>
2021-02-11 16:07:32 +0700

    feat(components): implement NavBar

---
+++ src/components/NavBar.test.tsx
@@ -0,0 +1,19 @@
+import { render } from 'utils/testUtils'
+import React from 'react'
+import { APP_NAME } from 'utils/constants'
+import NavBar from './NavBar'
+
+describe('NavBar', () => {
+    test('should render a logo and the app name', () => {
+        const { getByRole, getByText } = render(<NavBar />)
+        const logo = getByRole('img')
+        const appName = getByText(APP_NAME)
+        expect(logo).toBeInTheDocument()
+        expect(appName).toBeInTheDocument()
+    })
+
+    test('should match the most recent snapshot', () => {
+        const { asFragment } = render(<NavBar />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+})
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import React, { memo, PropsWithChildren } from 'react'
+import Navbar from 'react-bootstrap/NavBar'
+import Container from 'react-bootstrap/Container'
+import logo from 'static/img/logo.svg'
+import { APP_NAME } from 'utils/constants'
+import styles from './NavBar.module.scss'
+
+const NavBar = ({ children }: PropsWithChildren<unknown>) => (
+    <Navbar bg="dark" variant="dark" role="navigation">
+        <Container className={styles.navBarContainer}>
+            <Navbar.Brand href="/">
+                <img src={logo} alt="logo" width="32" />
+                {APP_NAME}
+            </Navbar.Brand>
+            {children}
+        </Container>
+    </Navbar>
+)
+
+export default memo(NavBar)
