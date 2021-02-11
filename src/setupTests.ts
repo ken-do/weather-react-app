33410154c46af314a54eb3623a7fc4290763b890
Ken Do <khuong.a.do@gmail.com>
2021-02-11 16:40:10 +0700

    chore: add a new matcher named  toMatchDiffSnapshot to jest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/naming-convention */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import 'snapshot-diff/extend-expect'
+import { toMatchDiffSnapshot as snapshotDiff } from 'snapshot-diff'
+
+declare global {
+    interface matchers<R> {
+        toMatchDiffSnapshot: typeof snapshotDiff
+    }
+}
