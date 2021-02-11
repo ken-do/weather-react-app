9327d5cad78486c79fd57e24d82ca1083bcb9d1a
Ken Do <khuong.a.do@gmail.com>
2021-02-11 16:16:30 +0700

    chore: add a few reusable types

--- src/types/data.ts
+++ src/types/data.ts
@@ -1,4 +1,4 @@
-interface ConsolidatedWeather {
+export interface ConsolidatedWeather {
     id: number
     weather_state_name: string
     weather_state_abbr: string
@@ -11,6 +11,25 @@ interface ConsolidatedWeather {
     wind_speed: number
     wind_direction: number
     air_pressure: number
+    humidity: number
+    visibility: number
+    predictability: number
+    [key: string]: unknown
+}
+
+export interface NormalizedConsolidatedWeather {
+    id: number
+    weatherStateName: string
+    weatherStateAbbr: string
+    windDirectionCompass: string
+    created: string
+    applicableDate: string
+    minTemp: number
+    maxTemp: number
+    theTemp: number
+    windSpeed: number
+    windDirection: number
+    airPressure: number
     humidity: number
     visibility: number
     predictability: number
@@ -28,6 +47,14 @@ export interface Location {
     location_type: string
     woeid: number
     latt_long: string
+    [key: string]: unknown
+}
+
+export interface NormalizedLocation {
+    title: string
+    locationType: string
+    woeid: number
+    lattLong: string
 }
 
 export interface LocationDetails {
@@ -42,5 +69,21 @@ export interface LocationDetails {
     location_type: string
     woeid: number
     latt_long: string
+    timezone: string
+    [key: string]: unknown
+}
+
+export interface NormalizedLocationDetails {
+    consolidatedWeather: ConsolidatedWeather[]
+    time: string
+    sunRise: string
+    sunSet: string
+    timezoneName: string
+    parent: Location
+    sources: ReportSource[]
+    title: string
+    locationType: string
+    woeid: number
+    lattLong: string
     timezone: string
 }
