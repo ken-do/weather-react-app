6749aa81de053fccef7e503662613d78287a8117
Ken Do <khuong.a.do@gmail.com>
2021-02-11 16:30:59 +0700

    chore(views): export all views from an index.ts file

--- src/router/views/index.ts
+++ src/router/views/index.ts
@@ -1,3 +1,4 @@
 export { default as Home } from './Home'
 export { default as NotFound } from './NotFound'
 export { default as Location } from './Location'
+export { default as Weather } from './Weather'
